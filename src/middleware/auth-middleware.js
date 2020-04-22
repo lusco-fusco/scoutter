b0d6f2b70f368b1b2284cbc0b11066e823e4809c
Luis Lorenzo <luislorenzomosquera@gmail.com>
2020-04-22 20:24:45 +0200

    X-API-KEY checker added as middleware module

---
+++ src/middleware/auth-middleware.js
@@ -0,0 +1,15 @@
+'user strict';
+
+require('dotenv').config();
+
+function checkXApiKey (req, res, next) {
+    key = req.header('x-api-key');
+    if (key === process.env.X_API_KEY || process.env.X_API_KEY) {
+        // keep executing the router middleware
+        next();
+    } else {
+        res.status(401).send('forbidden');
+    }
+}
+
+module.exports = checkXApiKey;
