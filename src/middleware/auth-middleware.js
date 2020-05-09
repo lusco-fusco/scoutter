55fbc4abaa3043f999f88f02cacb7d4c149a19ff
Luis Lorenzo <luislorenzomosquera@gmail.com>
2020-05-10 01:06:57 +0200

    last commit amend

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -3,7 +3,7 @@
 require("dotenv").config();
 
 function checkXApiKey(req, res, next) {
-  key = req.header("x-api-key");
+  let key = req.header("x-api-key");
   console.log(process.env.X_API_KEY);
   if (key === process.env.X_API_KEY) {
     // keep executing the router middleware
