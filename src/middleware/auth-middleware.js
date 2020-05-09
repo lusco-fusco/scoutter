cbd11b783f752f57e91421b8cb1e2dc1100796bc
Luis Lorenzo <luislorenzomosquera@gmail.com>
2020-05-10 01:03:48 +0200

    auth-middleware prettied

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,16 +1,16 @@
-'user strict';
+"user strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
-function checkXApiKey (req, res, next) {
-    key = req.header('x-api-key');
-    console.log(process.env.X_API_KEY)
-    if (key === process.env.X_API_KEY) {
-        // keep executing the router middleware
-        next();
-    } else {
-        res.status(401).send('forbidden');
-    }
+function checkXApiKey(req, res, next) {
+  key = req.header("x-api-key");
+  console.log(process.env.X_API_KEY);
+  if (key === process.env.X_API_KEY) {
+    // keep executing the router middleware
+    next();
+  } else {
+    res.status(401).send("forbidden");
+  }
 }
 
 module.exports = checkXApiKey;
